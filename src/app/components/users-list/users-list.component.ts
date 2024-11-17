dbd07634a6e5cbcef819bdf1624b75085d4e705e
YURKOV-PC\Yurkov <my_flexor@mail.ru>
2024-11-17 15:52:11 +0300

    Реализация таблицы списка пользователей

--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -1,12 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
+import { Table, TableModule } from 'primeng/table';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { User } from '../../models/models';
 
 @Component({
   selector: 'app-users-list',
   standalone: true,
   imports: [
     CommonModule,
+    TableModule,
+    IconFieldModule,
+    InputIconModule,
+    InputTextModule,
+    ButtonModule
   ],
   templateUrl: './users-list.component.html',
   styleUrl: './users-list.component.scss',
@@ -14,9 +25,32 @@ import { UserService } from '../../services/user.service';
   providers: [UserService]
 })
 export class UsersListComponent implements OnInit {
-  constructor(private userServise: UserService) {}
+  selectedUser: User | null = null;
+
+  constructor(public userService: UserService) {}
+
+  applyFilter(table: Table, event: any, matchMode: string) {
+    table.filterGlobal((event.target as HTMLInputElement).value, matchMode);
+    this.selectedUser = null;
+  }
 
   ngOnInit(): void {
-    this.userServise.getUsers();
+    this.userService.getUsers();
+  }
+
+  onAddClick(): void {
+    // TODO
+  }
+
+  onDeleteClick(): void {
+    // TODO
+  }
+
+  onDetailsClick(): void {
+    // TODO
+  }
+
+  onEditClick(): void {
+    // TODO
   }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { User } from '../models/models';
-import { catchError, tap, throwError } from 'rxjs';
+import { Address, Company, Geo, User } from '../models/models';
+import { BehaviorSubject, catchError, tap, throwError } from 'rxjs';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
 @UntilDestroy({ checkProperties: true })
@@ -9,6 +9,8 @@ import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
   providedIn: 'root'
 })
 export class UserService {
+  users$ = new BehaviorSubject<User[]>([]);
+
   private readonly baseUrl = 'https://jsonplaceholder.typicode.com/users';
 
   constructor(private http: HttpClient) {}
@@ -17,9 +19,25 @@ export class UserService {
     this.http.get<User[]>(this.baseUrl)
       .pipe(
         tap((response) => {
-          console.log(response);
+          // Если получили данные пользователей
+          if (response.length) {
+            const users: User[] = [];
+            // Для тестового отображения большого количества строк, добавляем полученные данные несколько раз
+            for (let i = 0; i < 10; i++) {
+              users.push(...JSON.parse(JSON.stringify(response)) as User[]);
+            }
+            // Правим идентификаторы для избавления от дублирования выделения строк
+            for (let i = 0; i < users.length; i++) {
+              users[i].id = i;
+            }
+            this.users$.next(users);
+          // Если полученные данные пустые, то генерируем тестовые данные
+          } else {
+            this.users$.next(this.getGeneratedUsers());
+          }
         }),
         catchError(() => {
+          this.users$.next(this.getGeneratedUsers());
           return throwError(() => new Error());
         }),
         untilDestroyed(this)
@@ -81,5 +99,50 @@ export class UserService {
         untilDestroyed(this)
       )
       .subscribe();
+  }
+
+  // Тестовое получение сгенерированного списка пользователей для случаев, когда внешнее API недоступно
+  private getGeneratedUsers(): User[] {
+    const users: User[] = [];
+
+    const randomString = (length: number) => {
+      let result = '';
+      const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      let counter = 0;
+      while (counter < length) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+        counter += 1;
+      }
+      return result;
+    };
+
+    for (let i = 0; i < 100; i++) {
+      const user: User = {
+        id: i,
+        name: `${randomString(10)} ${randomString(10)}`,
+        username: randomString(10),
+        email: `${randomString(10)}@${randomString(5)}`,
+        address: {
+          street: randomString(10),
+          suite: randomString(10),
+          city: randomString(10),
+          zipcode: randomString(5),
+          geo: {
+            lat: randomString(7),
+            lng: randomString(7)
+          } as Geo
+        } as Address,
+        phone: randomString(10),
+        website: randomString(10),
+        company: {
+          name: randomString(7),
+          catchPhrase: randomString(20),
+          bs: randomString(10),
+        } as Company
+      };
+      users.push(user);
+    }
+
+    return users;
   }
 }
